13cfd16b03e0e45bd519539860fc2d440099c96d
John Hur <hur.john628@gmail.com>
2022-03-07 04:10:01 -0600

    worked on resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -6,16 +6,68 @@ const resolvers = {
     Query: {
         me: async (parent, args, context) => {
             if (context.user) {
-                return User.findOne({ _id: context.user._id });
+                return User.findOne(
+                    [{ _id: user ? user._id : params.id } || { username: params.username }]
+                );
             }
             throw new AuthenticationError('Please log in.')
         },
     },
-    
+
     Mutation: {
-        createUser: async (parent, args) => {
-            const user = await User.create(args);
-            return user;
+        addUser: async (parent, {username, email, password}) => {
+            const user  = await User.create({ username, email, password });
+            const token = signToken(user);
+
+            return { token, user };
+        },
+
+        login: async (parent, {username, email, password}) => {
+            const user = await User.findOne({email} || {username});
+            
+            if (!user) {
+                throw new AuthenticationError('No user with that email or username')
+            };
+
+            const correctPw = await user.isCorrectPassword(password);
+
+            if (!correctPw) {
+                throw new AuthenticationError('Incorrect password.')
+            };
+
+            const token = signToken(user);
+            return { token, user }
+        },
+
+        saveBook: async (parent, { userId, book }, context) => {
+            if (context.user) {
+                return User.findOneAndUpdate(
+                    { _id: userId },
+                    {
+                        $addToSet: { savedBooks: book }
+                    },
+                    {
+                        new: true,
+                        runValidators: true,
+                    }
+                );
+            }
+            throw new AuthenticationError('You must be logged in to save a book.');
+        },
+
+        deleteBook: async (parent, { book }, context) => {
+            if (!context.user) {
+                return User.findOneAndDelete(
+                    { _id: context.user._id },
+                    {
+                        $pull: { savedBooks: book }
+                    },
+                    { new: true }
+                );
+            }
+            throw new AuthenticationError('You must be logged in to delete a saved book.')
         }
     }
-}
+};
+
+module.exports = resolvers;
