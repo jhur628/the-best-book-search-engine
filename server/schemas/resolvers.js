5a89043675a1badc7bbeef9a18b3403356a5d062
John Hur <hur.john628@gmail.com>
2022-03-07 03:27:15 -0600

    worked on resolvers.js a bit

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,7 +1,21 @@
+const { AuthenticationError } = require('apollo-server-express');
 const { Book, User } = require('../models');
+const { signToken } = require('../utils/auth');
 
 const resolvers = {
     Query: {
-        
+        me: async (parent, args, context) => {
+            if (context.user) {
+                return User.findOne({ _id: context.user._id });
+            }
+            throw new AuthenticationError('Please log in.')
+        },
+    },
+    
+    Mutation: {
+        createUser: async (parent, args) => {
+            const user = await User.create(args);
+            return user;
+        }
     }
 }
