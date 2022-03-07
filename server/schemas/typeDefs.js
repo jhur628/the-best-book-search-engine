ebf5ad3834294ffcca1141f1be310e8fc0a85a8d
John Hur <hur.john628@gmail.com>
2022-03-07 04:36:08 -0600

    added Auth and Query types in typeDef.js

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,19 +2,31 @@ const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
 type User {
-    _id: ID,
-    username: String,
-    email: String,
-    password: String,
-    savedBooks: [String]!
+    _id: ID
+    username: String
+    email: String
+    password: String
+    savedBooks: [Book]!
+    bookCount: Int
 }
 
 type Book {
-    authors: [String],
-    description: String,
-    bookId: String!,
-    image: String,
-    link: String,
-    title: String!  
+    authors: [String]
+    description: String
+    bookId: String!
+    image: String
+    link: String
+    title: String!
+}
+
+type Auth {
+    _id: ID!
+    user: User
+}
+
+type Query {
+    me: User
 }
 `
+
+//Input types: https://www.apollographql.com/docs/apollo-server/v2/schema/schema/#input-types
