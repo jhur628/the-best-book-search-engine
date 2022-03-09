0a9b2599d29b8a0a732ea083d47afcdf7e720e82
John Hur <hur.john628@gmail.com>
2022-03-09 04:38:15 -0600

    useQuery(GET_ME) added

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -16,7 +16,7 @@ const SearchBooks = () => {
   // create state to hold saved bookId values
   const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
 
-  const [ saveBook, { error } ] = useMutation(SAVE_BOOK);
+  const [ saveBook ] = useMutation(SAVE_BOOK);
 
   // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
   // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
@@ -60,7 +60,7 @@ const SearchBooks = () => {
   const handleSaveBook = async (bookId) => {
     // find the book in `searchedBooks` state by the matching id
     const bookToSave = searchedBooks.find((book) => book.bookId === bookId);
-
+    console.log(bookToSave);
     // get token
     const token = Auth.loggedIn() ? Auth.getToken() : null;
 
